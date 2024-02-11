fc38f985721567b2595fcb25b927c0129f636a3c
Altaha <altahabinsyafriadi@gmail.com>
2024-02-11 13:47:02 +0700

    2621. Sleep.js

---
+++ 2621. Sleep.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+  return new Promise((resolve, reject) =>
+    setTimeout(() => {
+      resolve(1);
+    }, millis)
+  );
+}
+
+let t = Date.now();
+sleep(100).then(() => console.log(Date.now() - t)); // 100
